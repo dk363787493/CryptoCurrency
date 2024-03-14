65a6f9ca18cf511b5dfff88516d9f045053311d5
VincentLee363787493 <vincent@coinfloww.com>
2024-03-14 23:25:08 +0800

    update main.go

--- demo/bitget/grabbingcrypto/main.go
+++ demo/bitget/grabbingcrypto/main.go
@@ -18,22 +18,22 @@ func main() {
 		ActionTime:    "2024-03-14 22:59:00",
 		SubTime:       200,
 	}
-	//s2 := &task.GrabbingTask{
-	//	ProfitRate:    0.05,
-	//	ApiKey:        "bg_8bb5b03eb0f08065b0442560441ed912",
-	//	SecretKey:     "a43174b22a98f0195f6d4ac887707210b3168cca92c44ae4ac6449ed4566d505",
-	//	Passphrase:    "Bsdk19901214123",
-	//	Symbol:        "ETHUSDT",
-	//	Amout:         "6",
-	//	PriceScale:    0.01,
-	//	QuantityScale: 0.0001,
-	//	ActionTime:    "2024-03-14 23:16:00",
-	//	SubTime:       200,
-	//}
+	s2 := &task.GrabbingTask{
+		ProfitRate:    0.05,
+		ApiKey:        "bg_02235de1865699e959ab82d4baf5dfd3",
+		SecretKey:     "d8fc4dfe4dff1a11a0c8f8c09b7d24ef3cefabc8539c32fc2e99e97a8cfe607a",
+		Passphrase:    "Bsdk19901214123",
+		Symbol:        "ETHUSDT",
+		Amout:         "6",
+		PriceScale:    0.01,
+		QuantityScale: 0.0001,
+		ActionTime:    "2024-03-14 23:16:00",
+		SubTime:       200,
+	}
 
 	tasks := make([]*task.GrabbingTask, 0)
 	tasks = append(tasks, s1)
-	//tasks = append(tasks, s2)
+	tasks = append(tasks, s2)
 	var wg sync.WaitGroup
 	for _, t := range tasks {
 		t.Init()
